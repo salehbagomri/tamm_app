7639dbd75690a0ee95b8e9183d3405034ebaf5cd
salehbagomri <s.bagomri@gmail.com>
2026-05-24 17:32:36 +0300

    chore: update support email to s.bagomri@gmail.com

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,5 +3,5 @@ class AppConstants {
 
   static const String supportPhone = '770727055';
   static const String supportWhatsApp = '967770727055';
-  static const String supportEmail = 'support@tamm-app.com';
+  static const String supportEmail = 's.bagomri@gmail.com';
 }
